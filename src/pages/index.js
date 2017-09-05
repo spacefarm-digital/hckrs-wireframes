9d5404fcd3ac8be270fe6a54714da7d7e283703f
kalina-ivanova <k@spacefarm.digital>
2017-09-05 21:56:59 +0300

    Add components folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import { Flex, Button, Text } from 'rebass'
+import React from "react";
+import Link from "gatsby-link";
+import { Flex, Button, Text } from "rebass";
+
+import MyCard from "../components/card";
 
 const IndexPage = () => (
   <div>
-    <Flex justify="flex-end">
+    <Flex justify="flex-start">
       <Button>Hey</Button>
-      <Text fontSize={1} bg="red">
+      <Text fontSize={1} bg="red" is="h1" bold w={1 / 2} center>
         A text thing
       </Text>
       <p>A text thing</p>
     </Flex>
+    <Link to="page-2">Click me</Link>
+    <MyCard />
   </div>
-)
+);
 
-export default IndexPage
+export default IndexPage;
