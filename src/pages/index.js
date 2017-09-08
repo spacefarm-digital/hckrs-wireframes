563fc264bfb79eb34613abd5ee897de58a7811b4
Darin Dimitroff <d@thecrazyones.agency>
2017-09-08 13:27:19 +0300

    Showcase ChallengePreview component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import { Flex, Button, Text, Container } from 'rebass'
-import AvatarCard from '../components/AvatarCard'
-import MyCard from '../components/Card'
+import ChallengePreview from '../components/ChallengePreview'
 import PrimaryNav from '../components/PrimaryNav'
 
 const IndexPage = () => (
   <div>
     <PrimaryNav />
-    <Container pt={5}>
-      <AvatarCard
-        image="https://static.esea.net/global/images/users/428235.1431669312.jpg"
-        name="Jane Doe"
-        title="boss lady"
+    <Container pt={3}>
+      <ChallengePreview
+        title="BigRed//Hacks"
+        category="👩‍⚕️"
+        image="https://s3.amazonaws.com/assets.mlh.io/events/splashes/000/000/684/thumb/bigred_splash.png?1500042878"
+        location="Cornell University"
+        excerpt="Across three days, students from all across the US will gather to build the coolest and most innovative projects, meet amazing people, and win some prizes along the way!"
+        participants={32}
+        date="September 15-17"
       />
     </Container>
   </div>
 )
-
 export default IndexPage
