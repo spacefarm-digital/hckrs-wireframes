c316e253d56cd5fc462bb6fe9162decbc0dc8478
Darin Dimitroff <d@thecrazyones.agency>
2017-09-11 20:49:07 +0300

    Update layout

--- src/components/ChallengeInfo.js
+++ src/components/ChallengeInfo.js
@@ -15,31 +15,30 @@ import PrettyLink from '../components/PrettyLink'
 
 const ChallengeInfo = props => (
   <Card wrap p={3}>
-    <Flex justify="space-between" mb={3} w={1}>
-      <Text bold>📅 11-13 Sep</Text>
+    <Flex justify="flex-end" mb={3} w={1}>
       <Text bold>Upcoming 🔵</Text>
     </Flex>
-    <Flex mb={3} w={1}>
+    <Flex mb={3} w={1} justify="center">
       <Avatar
-        size={72}
+        size={96}
         src="https://images.unsplash.com/photo-1462331940025-496dfbfc7564?w=2048&q=20"
-        mr={3}
       />
-      <Box>
-        <Heading f={3} mb={1} is="h1">
-          Challenge title
-        </Heading>
-        <Text>
-          Posted by: <PrettyLink>Challenger</PrettyLink>
-        </Text>
-      </Box>
+    </Flex>
+    <Flex column align="center" w={1}>
+      <Heading f={3} mb={1} is="h1">
+        Challenge title
+      </Heading>
+      <Text mb={3}>
+        Posted by: <PrettyLink>Challenger</PrettyLink>
+      </Text>
     </Flex>
     <Box w={1}>
-      <Flex w={1} mb={3} justify="space-between">
-        <Text>🗺 Sofia</Text>
-        <Text>🚀 Rocket engineering</Text>
-      </Flex>
-      <Heading is="h2" f={1} mb={2}>
+      <Text mb={3}>📅 11-13 Sep, 🗺 Sofia</Text>
+      <Text bold mb={1}>
+        Categories:
+      </Text>
+      <Text mb={3}>🚀 Rocket engineering, 👾 Technology, 👩‍⚕️ Health</Text>
+      <Heading is="h2" f={1} mb={1}>
         Prizes:
       </Heading>
       <ul>
