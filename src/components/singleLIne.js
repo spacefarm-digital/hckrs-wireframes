9eebe05c112baf7a09b6ed1da51ed17e63dd385e
Darin Dimitroff <d@thecrazyones.agency>
2017-09-11 14:27:39 +0300

    Add SingleLine component

---
+++ src/components/singleLIne.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Flex } from 'rebass'
+import styled from 'styled-components'
+
+const SingleLine = styled(Flex)`white-space: nowrap;`
+
+export default SingleLine
