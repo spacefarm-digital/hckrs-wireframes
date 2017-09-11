568ba715bdd321308bec801cdf460e5a490ee6e2
Darin Dimitroff <d@thecrazyones.agency>
2017-09-11 20:27:25 +0300

    Finish ChallengeInfo layout

--- src/components/ChallengeInfo.js
+++ src/components/ChallengeInfo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 import {
   Avatar,
   Heading,
@@ -7,32 +7,47 @@ import {
   Button,
   BackgroundImage,
   Circle,
-  Box
-} from "rebass";
-import Card from "../components/Card";
+  Box,
+} from 'rebass'
 
-import PrettyLink from "../components/PrettyLink";
+import Card from '../components/Card'
+import PrettyLink from '../components/PrettyLink'
 
 const ChallengeInfo = props => (
   <Card wrap p={3}>
-    <Flex>
-      <Avatar
-        size={64}
-        src="https://images.unsplash.com/photo-1462331940025-496dfbfc7564?w=2048&q=20"
-      />
-      <Heading f={4} mb={4} px={3}>
-        Challenge title
-      </Heading>
+    <Flex justify="space-between" mb={3} w={1}>
+      <Text bold>📅 11-13 Sep</Text>
+      <Text bold>Upcoming 🔵</Text>
     </Flex>
-    <Box>
-      <PrettyLink>Challenger</PrettyLink>
-      <Text>Upcoming 🔵</Text>
-      <Text>📅 11-13 Sept 2017</Text>
-      <Text>🗺 Sofia</Text>
-      <Text>🚀 Category</Text>
-      <Heading>Prizes</Heading>
-      <Text>1st place: $500</Text>
+    <Flex mb={3} w={1}>
+      <Avatar
+        size={72}
+        src="https://images.unsplash.com/photo-1462331940025-496dfbfc7564?w=2048&q=20"
+        mr={3}
+      />
+      <Box>
+        <Heading f={3} mb={1} is="h1">
+          Challenge title
+        </Heading>
+        <Text>
+          Posted by: <PrettyLink>Challenger</PrettyLink>
+        </Text>
+      </Box>
+    </Flex>
+    <Box w={1}>
+      <Flex w={1} mb={3} justify="space-between">
+        <Text>🗺 Sofia</Text>
+        <Text>🚀 Rocket engineering</Text>
+      </Flex>
+      <Heading is="h2" f={1} mb={2}>
+        Prizes:
+      </Heading>
+      <ul>
+        <Text is="li">🥇 $5000 + advisor support</Text>
+        <Text is="li">🥈 $2500 + advisor support</Text>
+        <Text is="li">🥉 $1000</Text>
+      </ul>
     </Box>
   </Card>
-);
-export default ChallengeInfo;
+)
+export default ChallengeInfo
--- src/pages/challenge-single.js
+++ src/pages/challenge-single.js
@@ -1,20 +1,19 @@
-import React from "react";
-import Link from "gatsby-link";
-import { Flex, Button, Text, Box, Heading } from "rebass";
+import React from 'react'
+import Link from 'gatsby-link'
+import { Flex, Button, Text, Box, Heading } from 'rebass'
 
-import PrimaryNav from "../components/PrimaryNav";
-import Restrain from "../components/Restrain";
-import ChallengeInfo from "../components/ChallengeInfo";
+import PrimaryNav from '../components/PrimaryNav'
+import Restrain from '../components/Restrain'
+import ChallengeInfo from '../components/ChallengeInfo'
 
 const ChallengesIndex = () => (
   <Restrain>
     <PrimaryNav />
-    {/* Title */}
-
     <Box px={3}>
-      {" "}
-      <ChallengeInfo />
+      <Box w={[1, 1 / 1.5, 1 / 2]}>
+        <ChallengeInfo />
+      </Box>
     </Box>
   </Restrain>
-);
-export default ChallengesIndex;
+)
+export default ChallengesIndex
