00c607e1d86e944c6eb5db232e9e96c1763aef99
Darin Dimitroff <d@thecrazyones.agency>
2017-09-11 14:28:40 +0300

    Add restrain component
    Temp. restraints stuff to 70em

---
+++ src/components/Restrain.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Box } from 'rebass'
+import styled from 'styled-components'
+
+const Restrain = styled(Box)`
+  max-width: 70em;
+  margin-left: auto;
+  margin-right: auto;
+`
+
+export default Restrain
