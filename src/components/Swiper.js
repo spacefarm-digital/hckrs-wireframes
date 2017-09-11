a307ab8180ef7f0c9c61fc85d379663d9d1069dd
Darin Dimitroff <d@thecrazyones.agency>
2017-09-11 14:28:19 +0300

    Make Swiper component full-width

--- src/components/Swiper.js
+++ src/components/Swiper.js
@@ -1,10 +1,13 @@
-import React from "react";
-import { Flex } from "rebass";
-import styled from "styled-components";
+import React from 'react'
+import { Flex } from 'rebass'
+import styled from 'styled-components'
 
 const Swiper = styled(Flex)`
   overflow-x: scroll;
   -webkit-overflow-scrolling: touch;
-`;
+  width: 100%;
+  position: relative;
+  white-space: nowrap;
+`
 
-export default Swiper;
+export default Swiper
