78dac470475618389b7100e97831416817b0dbae
Darin Dimitroff <d@thecrazyones.agency>
2017-09-14 14:10:50 +0300

    Add footer placeholder

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Box } from 'rebass'
+
+const Footer = () => (
+  <Box is="footer" bg="black" color="white" px={3} py={5}>
+    Footer placeholder
+  </Box>
+)
+
+export default Footer
